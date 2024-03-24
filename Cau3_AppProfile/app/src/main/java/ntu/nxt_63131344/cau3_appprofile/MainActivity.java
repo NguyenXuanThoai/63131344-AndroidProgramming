23a8a0ec4b0f0b0462f8651989d528a59b36e0c3
NguyenXuanThoai <nxuanthoai496@gmail.com>
2024-03-24 09:57:30 +0700

    Câu 3 thi giữa kỳ

---
+++ Cau3_AppProfile/app/src/main/java/ntu/nxt_63131344/cau3_appprofile/MainActivity.java
@@ -0,0 +1,14 @@
+package ntu.nxt_63131344.cau3_appprofile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
