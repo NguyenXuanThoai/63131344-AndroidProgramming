574fe33c73478db657e29562f324c62d64498c3e
NguyenXuanThoai <nxuanthoai496@gmail.com>
2024-03-25 11:48:26 +0700

    Update

--- Cau2_AppDoiTien/app/src/main/java/ntu/nxt_63131344/cau2_appdoitien/MainActivity.java
+++ Cau2_AppDoiTien/app/src/main/java/ntu/nxt_63131344/cau2_appdoitien/MainActivity.java
@@ -3,12 +3,24 @@ package ntu.nxt_63131344.cau2_appdoitien;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText edtUSD, edtEURO, edtVND;
+    Button btnChuyen, btnXoa;
 
+    void LayDl(){
+        edtUSD = findViewById(R.id.edtUSD);
+        edtVND = findViewById(R.id.edtVND);
+        edtEURO = findViewById(R.id.edtEUR);
+        btnChuyen = findViewById(R.id.buttonchuyen);
+        btnXoa = findViewById(R.id.btnXoa);
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LayDl();
     }
 }
