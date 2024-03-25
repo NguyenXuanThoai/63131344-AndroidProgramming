0fa8fa955d4bb6171db54d3151cbc52060438347
NguyenXuanThoai <nxuanthoai496@gmail.com>
2024-03-25 11:59:19 +0700

    Update MainActivity.java

--- Cau2_AppDoiTien/app/src/main/java/ntu/nxt_63131344/cau2_appdoitien/MainActivity.java
+++ Cau2_AppDoiTien/app/src/main/java/ntu/nxt_63131344/cau2_appdoitien/MainActivity.java
@@ -3,8 +3,10 @@ package ntu.nxt_63131344.cau2_appdoitien;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     EditText edtUSD, edtEURO, edtVND;
@@ -16,11 +18,33 @@ public class MainActivity extends AppCompatActivity {
         edtEURO = findViewById(R.id.edtEUR);
         btnChuyen = findViewById(R.id.buttonchuyen);
         btnXoa = findViewById(R.id.btnXoa);
+        btnChuyen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ChuyenDoiTien();
+            }
+        });
     }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         LayDl();
     }
+
+    void ChuyenDoiTien() {
+        String strusd = edtUSD.getText().toString();
+        if (strusd.isEmpty()) {
+            Toast.makeText(MainActivity.this, "Vui lòng nhập số tiền USD.", Toast.LENGTH_SHORT).show();
+            return;
+        }
+        double usd = Double.parseDouble(strusd);
+        double euro = usd * 0.92525;
+        double vnd = usd * 24767;
+
+        edtEURO.setText(String.valueOf(euro));
+        edtVND.setText(String.valueOf(vnd));
+    }
+
 }
