557886142663ddbf16cf7144ac9762ec89c5cca7
NguyenXuanThoai <nxuanthoai496@gmail.com>
2024-04-24 10:36:02 +0700

    VD2

---
+++ Bai1_VD2_LoginIntents/app/src/test/java/ntu/nxt_63131344/bai1_vd2_loginintents/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package ntu.nxt_63131344.bai1_vd2_loginintents;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
