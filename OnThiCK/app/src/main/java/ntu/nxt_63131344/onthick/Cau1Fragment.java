6cdedd4a91b5ef513ad84fecad586c9cd3c21235
NguyenXuanThoai <nxuanthoai496@gmail.com>
2024-06-02 11:14:50 +0700

    Update câu 1

--- OnThiCK/app/src/main/java/ntu/nxt_63131344/onthick/Cau1Fragment.java
+++ OnThiCK/app/src/main/java/ntu/nxt_63131344/onthick/Cau1Fragment.java
@@ -7,41 +7,22 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Cau1Fragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class Cau1Fragment extends Fragment {
-
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    EditText editText_met;
+    EditText editText_kilomet;
+    Button btnChuyen;
 
     public Cau1Fragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Cau1Fragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static Cau1Fragment newInstance(String param1, String param2) {
         Cau1Fragment fragment = new Cau1Fragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
     }
@@ -49,16 +30,51 @@ public class Cau1Fragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_cau1, container, false);
+
+        View viewCau1 = inflater.inflate(R.layout.fragment_cau1, container, false);
+
+        // Tìm điều khiển
+        editText_met = viewCau1.findViewById(R.id.txtmet);
+        editText_kilomet = viewCau1.findViewById(R.id.txtkm);
+        btnChuyen  = viewCau1.findViewById(R.id.btnchuyen);
+
+        btnChuyen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String duLieuMet = editText_met.getText().toString();
+                String duLieuKiloMet = editText_kilomet.getText().toString();
+
+                if (!duLieuMet.isEmpty()) {
+                     // met sang km
+                    try {
+                        double met = Double.parseDouble(duLieuMet);
+                        double kilomet = met / 1000;
+                        editText_kilomet.setText(String.valueOf(kilomet));
+                        Toast.makeText(viewCau1.getContext(), "m -> km", Toast.LENGTH_SHORT).show();
+                    } catch (NumberFormatException e) {
+                        Toast.makeText(viewCau1.getContext(), "Vui lòng nhập số hợp lệ", Toast.LENGTH_SHORT).show();
+                    }
+                } else if (!duLieuKiloMet.isEmpty()) {
+                    // Đổi từ km sang met
+                    try {
+                        double kilomet = Double.parseDouble(duLieuKiloMet);
+                        double met = kilomet * 1000;
+                        editText_met.setText(String.valueOf(met));
+                        Toast.makeText(viewCau1.getContext(), "km -> m", Toast.LENGTH_SHORT).show();
+                    } catch (NumberFormatException e) {
+                        Toast.makeText(viewCau1.getContext(), "Vui lòng nhập số hợp lệ", Toast.LENGTH_SHORT).show();
+                    }
+                } else {
+                    Toast.makeText(viewCau1.getContext(), "Vui lòng nhập dữ liệu để chuyển đổi", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        return viewCau1;
     }
 }
